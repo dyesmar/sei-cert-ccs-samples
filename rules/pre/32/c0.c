e6afdafb0b4b2eb2b467f91c8200a619cf8c8ae0
Ramsey Dow <yesmar@gmail.com>
2019-08-23 13:59:48 -0700

    Add PRE32-C rule

---
+++ rules/pre/32/c0.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+void func(const char *src) {
+  /* Validate the source string; calculate size */
+  char *dest;
+  /* malloc() destination string */ 
+  #ifdef PLATFORM1
+    memcpy(dest, src, 12);
+  #else
+    memcpy(dest, src, 24);
+  #endif
+  /* ... */
+}
---
+++ rules/pre/32/nc0.c
@@ -0,0 +1,15 @@
+#include <string.h>
+ 
+void func(const char *src) {
+  /* Validate the source string; calculate size */
+  char *dest;
+  /* malloc() destination string */ 
+  memcpy(dest, src,
+    #ifdef PLATFORM1
+      12
+    #else
+      24
+    #endif
+  );
+  /* ... */
+}
