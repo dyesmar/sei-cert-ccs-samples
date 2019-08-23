6e17216a7c610e07eb80b6aa952348c9894a4e4d
Ramsey Dow <yesmar@gmail.com>
2019-08-23 14:02:58 -0700

    Add EXP42-C rule

---
+++ rules/exp/42/nc0.c
@@ -0,0 +1,14 @@
+#include <string.h>
+  
+struct s {
+  char c;
+  int i;
+  char buffer[13];
+};
+  
+void compare(const struct s *left, const struct s *right) { 
+  if ((left && right) &&
+      (0 == memcmp(left, right, sizeof(struct s)))) {
+    /* ... */
+  }
+}
