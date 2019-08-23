d8059edf3913ffbf3a17362f6a18ebf8016cdf2c
Ramsey Dow <yesmar@gmail.com>
2019-08-23 16:05:59 -0700

    Add STR37-C rule

---
+++ rules/str/37/nc0.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include <string.h>
+  
+size_t count_preceding_whitespace(const char *s) {
+  const char *t = s;
+  size_t length = strlen(s) + 1;
+  while (isspace(*t) && (t - s < length)) {
+    ++t;
+  }
+  return t - s;
+} 
