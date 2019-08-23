f601b46ae80d33303348c811448923365d7cb238
Ramsey Dow <yesmar@gmail.com>
2019-08-23 14:00:22 -0700

    Add DCL36-C rule

---
+++ rules/dcl/36/c0.c
@@ -0,0 +1,10 @@
+int i1 = 10;         /* Definition, external linkage */
+static int i2 = 20;  /* Definition, internal linkage */
+extern int i3 = 30;  /* Definition, external linkage */
+int i4;              /* Tentative definition, external linkage */
+static int i5;       /* Tentative definition, internal linkage */
+ 
+int main(void) {
+  /* ... */
+  return 0;
+}
