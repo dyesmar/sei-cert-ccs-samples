3e88f5b4fcc249d64f7de52ea89c1f444cff394c
Ramsey Dow <yesmar@gmail.com>
2019-08-24 11:33:44 -0700

    Add FIO40-C rule

---
+++ rules/fio/40/nc0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+  
+enum { BUFFER_SIZE = 1024 };
+void func(FILE *file) {
+  char buf[BUFFER_SIZE];
+ 
+  if (fgets(buf, sizeof(buf), file) == NULL) {
+    /* Set error flag and continue */
+  }
+}
