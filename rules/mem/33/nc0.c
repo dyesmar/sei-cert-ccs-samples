0cd5bab9c3413901c1a12c1872803a0385697234
Ramsey Dow <yesmar@gmail.com>
2019-08-24 11:00:37 -0700

    Add MEM33-C rule

---
+++ rules/mem/33/nc0.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+  
+struct flex_array_struct {
+  size_t num;
+  int data[];
+};
+  
+void func(void) {
+  struct flex_array_struct flex_struct;
+  size_t array_size = 4;
+ 
+  /* Initialize structure */
+  flex_struct.num = array_size;
+ 
+  for (size_t i = 0; i < array_size; ++i) {
+    flex_struct.data[i] = 0;
+  }
+}
