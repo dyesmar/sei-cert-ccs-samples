b79c9e69e6bb7ca23e03044ceda5f3ed88becbd6
Ramsey Dow <yesmar@gmail.com>
2019-08-24 15:32:56 -0700

    Add ENV34-C rule

---
+++ rules/env/34/c2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+  
+void func(void) {
+  char *tmpvar;
+  char *tempvar;
+  size_t len;
+ 
+  errno_t err = _dupenv_s(&tmpvar, &len, "TMP");
+  if (err) {
+    /* Handle error */
+  }
+  err = _dupenv_s(&tempvar, &len, "TEMP");
+  if (err) {
+    /* Handle error */
+  }
+ 
+  if (strcmp(tmpvar, tempvar) == 0) {
+    printf("TMP and TEMP are the same.\n");
+  } else {
+    printf("TMP and TEMP are NOT the same.\n");
+  }
+  free(tmpvar);
+  tmpvar = NULL;
+  free(tempvar);
+  tempvar = NULL;
+}
---
+++ rules/env/34/nc0.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+  
+void func(void) {
+  char *tmpvar;
+  char *tempvar;
+ 
+  tmpvar = getenv("TMP");
+  if (!tmpvar) {
+    /* Handle error */
+  }
+  tempvar = getenv("TEMP");
+  if (!tempvar) {
+    /* Handle error */
+  }
+  if (strcmp(tmpvar, tempvar) == 0) {
+    printf("TMP and TEMP are the same.\n");
+  } else {
+    printf("TMP and TEMP are NOT the same.\n");
+  }
+}
