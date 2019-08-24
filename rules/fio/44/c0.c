de204e70cc32e38292749289f6a8cc1ec3bb2652
Ramsey Dow <yesmar@gmail.com>
2019-08-24 11:41:22 -0700

    Add FIO44-C rule

---
+++ rules/fio/44/c0.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+  
+int opener(FILE *file) {
+  int rc;
+  fpos_t offset;
+ 
+  if (file == NULL) {
+    return -1;
+  }
+ 
+  rc = fgetpos(file, &offset);
+  if (rc != 0 ) {
+    return rc;
+  }
+ 
+  /* Read in data from file */
+ 
+  rc = fsetpos(file, &offset);
+  if (rc != 0 ) {
+    return rc;
+  }
+ 
+  return 0;
+}
---
+++ rules/fio/44/nc0.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+  
+int opener(FILE *file) {
+  int rc;
+  fpos_t offset;
+ 
+  memset(&offset, 0, sizeof(offset));
+ 
+  if (file == NULL) {
+    return -1;
+  }
+ 
+  /* Read in data from file */
+ 
+  rc = fsetpos(file, &offset);
+  if (rc != 0 ) {
+    return rc;
+  }
+ 
+  return 0;
+}
