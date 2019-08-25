4073c513853597921d72fa6075e5793961295c85
Ramsey Dow <yesmar@gmail.com>
2019-08-25 16:07:01 -0700

    Add CON36-C rule

---
+++ rules/con/36/nc0.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <threads.h>
+  
+struct node_t {
+  void *node;
+  struct node_t *next;
+};
+  
+struct node_t list;
+static mtx_t lock;
+static cnd_t condition;
+  
+void consume_list_element(void) {
+  if (thrd_success != mtx_lock(&lock)) {
+    /* Handle error */
+  }
+  
+  if (list.next == NULL) {
+    if (thrd_success != cnd_wait(&condition, &lock)) {
+      /* Handle error */
+    }
+  }
+ 
+  /* Proceed when condition holds */
+ 
+  if (thrd_success != mtx_unlock(&lock)) {
+    /* Handle error */
+  }
+}
