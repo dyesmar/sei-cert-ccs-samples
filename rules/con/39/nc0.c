24f6a8ce670f321670f3d527e250573beeadc2e7
Ramsey Dow <yesmar@gmail.com>
2019-08-25 16:12:20 -0700

    Add CON39-C rule

---
+++ rules/con/39/nc0.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <threads.h>
+  
+int thread_func(void *arg) {
+  /* Do work */
+  thrd_detach(thrd_current());
+  return 0;
+}
+ 
+int main(void) {
+  thrd_t t;
+ 
+  if (thrd_success != thrd_create(&t, thread_func, NULL)) {
+    /* Handle error */
+    return 0;
+  }
+ 
+  if (thrd_success != thrd_join(t, 0)) {
+    /* Handle error */
+    return 0;
+  }
+  return 0;
+}
