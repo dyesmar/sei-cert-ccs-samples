d4da9b07ab0ac2368c4acdcc3cd600d4552079a9
Ramsey Dow <yesmar@gmail.com>
2019-08-25 16:09:05 -0700

    Add CON37-C rule

---
+++ rules/con/37/c0.c
@@ -0,0 +1,26 @@
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <threads.h>
+  
+atomic_bool flag = ATOMIC_VAR_INIT(false);
+ 
+int func(void *data) {
+  while (!flag) {
+    /* ... */
+  }
+  return 0;
+}
+ 
+int main(void) {
+  thrd_t tid;
+   
+  if (thrd_success != thrd_create(&tid, func, NULL)) {
+    /* Handle error */
+  }
+  /* ... */
+  /* Set flag when done */
+  flag = true;
+ 
+  return 0;
+}
---
+++ rules/con/37/nc0.c
@@ -0,0 +1,28 @@
+#include <signal.h>
+#include <stddef.h>
+#include <threads.h>
+  
+volatile sig_atomic_t flag = 0;
+ 
+void handler(int signum) {
+  flag = 1;
+}
+ 
+/* Runs until user sends SIGUSR1 */
+int func(void *data) {
+  while (!flag) {
+    /* ... */
+  }
+  return 0;
+}
+ 
+int main(void) {
+  signal(SIGUSR1, handler); /* Undefined behavior */
+  thrd_t tid;
+   
+  if (thrd_success != thrd_create(&tid, func, NULL)) {
+    /* Handle error */
+  }
+  /* ... */
+  return 0;
+}
