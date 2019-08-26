73e976457e378790e801c5d06f3f44bd453591f3
Ramsey Dow <yesmar@gmail.com>
2019-08-26 11:56:33 -0700

    Add POS35-C rule

---
+++ rules/pos/35/c0.c
@@ -0,0 +1,28 @@
+char *filename = /* file name */;
+char *userbuf = /* user data */;
+unsigned int userlen = /* length of userbuf string */;
+ 
+struct stat lstat_info;
+struct stat fstat_info;
+int fd;
+/* ... */
+if (lstat(filename, &lstat_info) == -1) {
+  /* handle error */
+}
+ 
+fd = open(filename, O_RDWR);
+if (fd == -1) {
+  /* handle error */
+}
+ 
+if (fstat(fd, &fstat_info) == -1) {
+  /* handle error */
+}
+ 
+if (lstat_info.st_mode == fstat_info.st_mode &&
+    lstat_info.st_ino == fstat_info.st_ino  &&
+    lstat_info.st_dev == fstat_info.st_dev) {
+  if (write(fd, userbuf, userlen) < userlen) {
+    /* Handle Error */
+  }
+}
---
+++ rules/pos/35/nc0.c
@@ -0,0 +1,20 @@
+char *filename = /* file name */;
+char *userbuf = /* user data */;
+unsigned int userlen = /* length of userbuf string */;
+ 
+struct stat lstat_info;
+int fd;
+/* ... */
+if (lstat(filename, &lstat_info) == -1) {
+  /* Handle error */
+}
+ 
+if (!S_ISLNK(lstat_info.st_mode)) {
+   fd = open(filename, O_RDWR);
+   if (fd == -1) {
+       /* Handle error */
+   }
+}
+if (write(fd, userbuf, userlen) < userlen) {
+  /* Handle error */
+}
