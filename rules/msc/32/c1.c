c752ee42690c095dd6b3c4bdc604b72fa48cf535
Ramsey Dow <yesmar@gmail.com>
2019-08-26 07:25:02 -0700

    Add MSC32-C rule

---
+++ rules/msc/32/c1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <Bcrypt.h>
+#include <Ntstatus.h>
+#include <Wincrypt.h>
+ 
+void func(void) {
+  BCRYPT_ALG_HANDLE hAlgorithm = NULL;
+  long rand_buf;
+  PUCHAR pbBuffer = (PUCHAR) &rand_buf;
+  ULONG cbBuffer = sizeof(rand_buf);
+  ULONG dwFlags = BCRYPT_USE_SYSTEM_PREFERRED_RNG;
+  NTSTATUS status;
+  for (unsigned int i = 0; i < 10; ++i) {
+    status = BCryptGenRandom(hAlgorithm, pbBuffer, cbBuffer, dwFlags);
+    if (status == STATUS_SUCCESS) {
+      printf("%ld, ", rand_buf);
+    } else {
+      /* Handle Error */
+    }
+  }
+}
