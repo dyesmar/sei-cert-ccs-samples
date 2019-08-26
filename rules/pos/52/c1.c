7e2bed20b4881971fd68fcd50024c4b6a91f7289
Ramsey Dow <yesmar@gmail.com>
2019-08-26 12:14:16 -0700

    Add POS52-C rule

---
+++ rules/pos/52/c1.c
@@ -0,0 +1,20 @@
+void thread_foo(void *ptr) {
+  uint32_t num;
+  int result;
+ 
+  /* sock is a connected TCP socket */
+ 
+  if ((result = recv(sock, (void *)&num, sizeof(uint32_t), O_NONBLOCK)) < 0) {
+    /* Handle Error */
+  }
+ 
+  if ((result = pthread_mutex_lock(&mutex)) != 0) {
+    /* Handle Error */
+  }
+ 
+  /* ... */
+ 
+  if ((result = pthread_mutex_unlock(&mutex)) != 0) {
+    /* Handle Error */
+  }
+}
