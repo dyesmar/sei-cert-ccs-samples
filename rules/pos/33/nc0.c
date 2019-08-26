c6df52079f84107e0adb7239205497703dfe6377
Ramsey Dow <yesmar@gmail.com>
2019-08-26 11:53:08 -0700

    Add POS33-C rule

---
+++ rules/pos/33/nc0.c
@@ -0,0 +1,9 @@
+char *filename = /* something */;
+ 
+pid_t pid = vfork();
+ if (pid == 0 )  /* child */ {
+   if (execve(filename, NULL, NULL) == -1) {
+     /* Handle error */
+   }
+   _exit(1);  /* in case execve() fails */
+}
