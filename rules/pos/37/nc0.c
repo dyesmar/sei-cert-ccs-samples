00e4f298cd9f86874765ddabc016d20c035681bb
Ramsey Dow <yesmar@gmail.com>
2019-08-26 11:59:38 -0700

    Add POS37-C rule

---
+++ rules/pos/37/nc0.c
@@ -0,0 +1,30 @@
+/* Code intended to run with elevated privileges */
+ 
+/* Temporarily drop privileges */
+if (seteuid(getuid()) != 0) {
+  /* Handle error */
+}
+ 
+/* Code intended to run with lower privileges */
+ 
+if (need_more_privileges) {
+  /* Restore privileges */
+  if (seteuid(0) != 0) {
+    /* Handle error */
+  }
+ 
+  /* Code intended to run with elevated privileges */
+}
+ 
+/* ... */
+ 
+/* Permanently drop privileges */
+if (setuid(getuid()) != 0) {
+  /* Handle error */
+}
+ 
+/*
+ * Code intended to run with lower privileges,
+ * but if privilege relinquishment failed,
+ * attacker can regain elevated privileges!
+ */
