f18164cf5f3b1739ebdbf72fb00c5185ea7bd652
Ramsey Dow <yesmar@gmail.com>
2019-08-26 12:16:55 -0700

    Add POS54-C rule

---
+++ rules/pos/54/nc0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+  
+int main(int argc, char *argv[]) {
+  FILE *out;
+  FILE *in;
+  size_t size;
+  char *ptr;
+  
+  if (argc != 2) {
+    /* Handle error */
+  }
+  
+  in = fmemopen(argv[1], strlen(argv[1]), "r");
+  /* Use in */
+  
+  out = open_memstream(&ptr, &size);
+  /* Use out */
+  
+  return 0;
+}
